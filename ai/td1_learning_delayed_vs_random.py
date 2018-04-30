9639ec79b9bf14a2aa40c6a2fd26a5278212b17f
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-04-30 09:49:37 -0400

    updating TD1 learning algorithm

--- ai/td1_learning_delayed_vs_random.py
+++ ai/td1_learning_delayed_vs_random.py
@@ -1,10 +1,10 @@
 from ai import *
 
 board_size = (2, 2)
-num_episodes = 500000
+num_episodes = 100000
 learning_rate = 1.0
 gamma = 0.99
-epsilon = 0.18
+epsilon = 0.0
 
 print("initializing value table for (%s, %s) game..." % (board_size[0], board_size[1]))
 
@@ -18,8 +18,6 @@ def compute_reward(game):
         return 1.0
     return 0.0
 
-total_rewards = []
-results = {'won': 0, 'lost': 0, 'tied': 0}
 
 for episode_num in range(1, num_episodes + 1):
     players = ['p1', 'random']
@@ -41,20 +39,33 @@ for episode_num in range(1, num_episodes + 1):
     reward = compute_reward(game)
     p1.update_value(reward, backups)
     # analyze results
-    total_rewards.append(reward)
-    p1_score = game.get_score('p1')
-    random_score = game.get_score('random')
-    if p1_score > random_score:
-        results['won'] += 1
-    elif p1_score < random_score:
-        results['lost'] += 1
-    else:
-        results['tied'] += 1
-    if episode_num % 100 == 0:
-        vt = p1.get_value_table()
-        print("%s, %s, %s, %s" % (episode_num, sum(total_rewards), results, len([x for x in vt if vt[x] > 0.0 ])))
-        total_rewards = []
+    if episode_num % 500 == 0:
+        # play against random opponent
         results = {'won': 0, 'lost': 0, 'tied': 0}
+        for trial in range(500):
+            players = ['policy', 'random']
+            if trial % 2 == 0:
+                players = [x for x in reversed(players)]
+            game = Game(board_size, players)
+            current_player = game.get_current_player()
+            while not game.is_finished():
+                board_state = game.get_board_state()
+                if current_player == 'random':
+                    edge = random_policy.select_edge(board_state)
+                    current_player, _ = game.select_edge(edge, 'random')
+                else:
+                    edge = p1.select_edge(board_state)
+                    current_player, _ = game.select_edge(edge, 'policy')
+            policy_score = game.get_score('policy')
+            random_score = game.get_score('random')
+            if policy_score > random_score:
+                results['won'] += 1
+            elif random_score > policy_score:
+                results['lost'] += 1
+            else:
+                results['tied'] += 1
+        vt = p1.get_value_table()
+        print("%s, %s, %s, %s" % (episode_num, results['won'], results, len([x for x in vt if vt[x] > 0.0 ])))
 
 
 print(p1.get_value_table())
