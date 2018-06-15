edc5f19bb2cade737f360aa80393d488201e0e26
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-06-15 09:53:33 -0400

    changing the plotting style

--- util/plotter.py
+++ util/plotter.py
@@ -3,10 +3,10 @@ import pylab
 
 data = pylab.loadtxt('../out/plotdata.txt', delimiter=',', usecols=(0, 1))
 
-pylab.plot(data[:, 0], data[:, 1], 'ko-', linewidth=3)
+pylab.plot(data[:, 0], data[:, 1], 'k-', linewidth=0.5, marker='.', markersize=2)
 
 plt.xlabel("iteration", size=15)
 plt.ylabel("performance", size=15)
-plt.grid(True)
+plt.grid(True, linewidth=0.2)
 
 plt.show()
